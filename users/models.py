cb90a64ce92dc2fe2372d7f5b20e589f3e4d06c7
Izzy Kim <kimizzy@seas.upenn.edu>
2022-04-30 09:49:53 +0900

    Users 모델 만들기

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    GENDER_CHOICES = (("male", "Male"), ("female", "Female"))
+    gender = models.CharField(max_length=10, blank=True, choices = GENDER_CHOICES)  #blank = 비어도 되는지
+    phone_number = models.CharField(max_length=20)
+    is_owner = models.BooleanField(default=False)
