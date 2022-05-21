729da09125d2b287846a805e7f17e55ac657cd32
Izzy Kim <kimizzy@seas.upenn.edu>
2022-05-21 11:01:10 +0900

    Place detail

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
+    path("places/", include("places.urls", namespace="places")),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- places/views.py
+++ places/views.py
@@ -4,3 +4,7 @@ from . import models
 def all_places(request):
     places = models.Place.objects.all()
     return render(request, "home.html", context={"places": places, "wow": 17456})
+
+def place_detail(request, pk):
+    place = models.Place.objects.get(pk=pk)
+    return render(request, "places/detail.html", context={"place": place})
