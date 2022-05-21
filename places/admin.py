6fdcc233209caa9eb3fa7aeebe8a6ba1e5fca0a8
Izzy Kim <kimizzy@seas.upenn.edu>
2022-05-21 10:40:41 +0900

    Place - Photo relationship established

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 
-@admin.register(models.Place)
+@admin.register(models.Place, models.Photo)
 class CustomPlaceAdmin(admin.ModelAdmin):
     pass
+
--- places/models.py
+++ places/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from core.models import TimeStampedModel
 from users.models import User
 
-# Create your models here.
 class Place(TimeStampedModel):
     name = models.CharField(max_length=50, blank=False)
     address = models.CharField(max_length=255, blank=False)
@@ -14,5 +13,18 @@ class Place(TimeStampedModel):
 
     def __str__(self):
         return self.name + "-" + self.owner.username
+    
+    def thumbnail(self):
+        if len(self.photos.all()) >0:
+            return self.photos.all()[0].file.url
+        else:
+            return None
+
+class Photo(TimeStampedModel):
+    file = models.ImageField(blank=True, upload_to="static/img/place_photos")
+    caption = models.CharField(blank=True, max_length=100)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="photos")
+
+   
 
 
