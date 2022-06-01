111f0267a5bc7e5472912d7e18bda61660683cbd
Izzy Kim <kimizzy@seas.upenn.edu>
2022-06-01 15:14:20 +0900

    Split places into page views

--- places/views.py
+++ places/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from . import models
 
 def all_places(request):
+    page = int(request.GET.get("page", 1))
     places = models.Place.objects.all()
-    return render(request, "home.html", context={"places": places, "wow": 17456})
+    start = (page - 1) * 10
+    return render(request, "home.html", context={"places": places[start:start+10]})
 
 def place_detail(request, pk):
     place = models.Place.objects.get(pk=pk)
